8170476f05c86b5df0bb8fb6e48a8a51ca263df1
Leonardo Steffen <steffencrespo@hotmail.com>
2016-02-16 11:09:50 -0800

    Database - adds To-Go model and RESTful API routes find, create and delete places to go

--- server.js
+++ server.js
@@ -17,6 +17,68 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+var Togo = mongoose.model('Togo', {
+    name : String,
+    url: String,
+    rating: String
+
+});
+
+// routes ======================================================================
+
+// api ---------------------------------------------------------------------
+// get all Togos
+app.get('/api/togos', function(req, res) {
+
+    // use mongoose to get all togos in the database
+    Togo.find(function(err, togos) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+
+        res.json(togos); // return all togos in JSON format
+    });
+});
+
+// create togo and send back all togos after creation
+app.post('/api/togos', function(req, res) {
+
+    // create a togo, information comes from AJAX request from Angular
+    Togo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, togo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the togos after you create another
+        Togo.find(function(err, togos) {
+            if (err)
+                res.send(err)
+            res.json(togos);
+        });
+    });
+
+});
+
+// delete a togo
+app.delete('/api/togos/:togo_id', function(req, res) {
+    Togo.remove({
+        _id : req.params.togo_id
+    }, function(err, togo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the togos after you create another
+        Togo.find(function(err, togos) {
+            if (err)
+                res.send(err)
+            res.json(togos);
+        });
+    });
+});
+
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
